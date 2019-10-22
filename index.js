70a66037000da3475dd7eeb8cf154ddefe90f887
SnO2WMaN <sno2wman.official@gmail.com>
2019-10-23 02:37:40 +0900

    🐛 Fix preset name transforming

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import { gitmojis } from "./gitmoji/src/data/gitmojis.json";
 
 module.exports = gitmojis.map(({ emoji, code, description }) => ({
   emoji,
-  name: code.substring(1, code.length),
+  name: code.substring(1, code.length - 1),
   description
 }));
